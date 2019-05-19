beaeca8d39143759bf8c3e709f997d24c667be56
gds21 <gds721@gmail.com>
2019-05-19 17:22:30 +0800

    Whitelist IP

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :apps, controller: :app_data
-  resources :items
-  resources :lists
+  resources :apps, controller: :app_data, constraints: lambda { |req|
+    ["220.133.42.141"].include?(req.remote_ip)}
+  resources :items,constraints: lambda { |req|
+    ["220.133.42.141"].include?(req.remote_ip)}
+  resources :lists,constraints: lambda { |req|
+    ["220.133.42.141"].include?(req.remote_ip)}
 
   put "image", to: "image#create"
 end
