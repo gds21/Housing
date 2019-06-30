05c0eef2466db56d816854453e63019b2e4df3b2
gds21 <gds721@gmail.com>
2019-06-30 15:33:18 +0800

    allow put

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Housing
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :patch, :delete ,:options]
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :delete, :put, :options]
       end
     end
 
