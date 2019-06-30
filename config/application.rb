a412c43e37cbec515c9f2405a757fe76efde1773
gds21 <gds721@gmail.com>
2019-06-30 14:44:20 +0800

    CORS

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,14 @@ module Housing
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Taipei'
+    config.active_record.default_timezone = :local
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :delete ,:options]
+      end
+    end
   end
 end
